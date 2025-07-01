36fbadbe90348080f72df12a80c91eb622a0352f
Yusif Aliyev <114253515+YusifAliyevPro@users.noreply.github.com>
2025-07-01 13:09:31 +0400

    feat: centralize admin email reference and update related checks

--- src/data-access/omdb/actions.ts
+++ src/data-access/omdb/actions.ts
@@ -1,10 +1,11 @@
 "use server";
 
 import { auth, signIn } from "@/lib/auth";
+import { AdminEmail } from "@/lib/constants";
 
 export async function getOMDBDataById(imdbID: string) {
   const session = await auth();
-  if (!session || session.user?.email !== "yusifaliyevpro@gmail.com") await signIn("github");
+  if (!session || session.user?.email !== AdminEmail) await signIn("github");
   const OMDB_API_KEY = process.env.OMDB_API_KEY;
   const response = await fetch(`https://www.omdbapi.com/?i=${imdbID}&apikey=${OMDB_API_KEY}`);
   return await response.json();
