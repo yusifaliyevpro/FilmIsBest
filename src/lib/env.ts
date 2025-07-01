7931cc3cc0f8406c98f0c8af4b9f5ab66cabf564
Yusif Aliyev <114253515+YusifAliyevPro@users.noreply.github.com>
2025-07-01 13:36:52 +0400

    feat: add AUTH_TRUST_HOST to environment schema

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -10,6 +10,7 @@ const EnvSchema = z.object({
   AUTH_GITHUB_ID: z.string().trim().min(3),
   AUTH_GITHUB_SECRET: z.string().trim().min(3),
   ADMIN_EMAIL: z.string().trim().min(3),
+  AUTH_TRUST_HOST: z.literal("true"),
 });
 
 type EnvSchemaType = z.infer<typeof EnvSchema>;
