0acb746bcc49c70fb0768af20ad9cc4c4a31cc27
Yusif Aliyev <114253515+YusifAliyevPro@users.noreply.github.com>
2025-07-15 13:52:14 +0400

    Refactor layout and styles: remove unused CSS files and streamline layout components

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,33 +1,18 @@
 import "./admin.css";
 import { Providers } from "@/components/Providers";
-import { BASE_URL } from "@/lib/constants";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
-import { Inter } from "next/font/google";
 import { ReactNode } from "react";
-
-const inter = Inter({ subsets: ["latin"] });
-export const metadata = {
-  metadataBase: new URL(BASE_URL),
-  title: {
-    default: "FilmIsBest",
-    template: "FilmIsBest | %s",
-  },
-};
 
 export default async function RootLayout({ children }: { children: Readonly<ReactNode> }) {
   const messages = await getMessages();
   return (
-    <html className="a bg-gray-100 dark" lang="az">
-      <body className={inter.className}>
-        <NextIntlClientProvider messages={messages}>
-          <Providers>
-            <main className="relative flex min-h-screen flex-col items-center justify-start bg-white text-black">
-              {children}
-            </main>
-          </Providers>
-        </NextIntlClientProvider>
-      </body>
-    </html>
+    <NextIntlClientProvider messages={messages}>
+      <Providers>
+        <main className="relative flex min-h-screen flex-col items-center justify-start bg-white text-black">
+          {children}
+        </main>
+      </Providers>
+    </NextIntlClientProvider>
   );
 }
