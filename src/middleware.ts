4518f0ee373b507fdfb78f2655afee371db570d3
Yusif Aliyev <114253515+YusifAliyevPro@users.noreply.github.com>
2025-10-15 22:26:37 +0400

    Exclude /studio from middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,5 +4,6 @@ import createMiddleware from "next-intl/middleware";
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
+  // Except /studio
+  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*|studio).*)",
 };
