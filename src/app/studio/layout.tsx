a6190f4eab09b4aa1b3bfc072c0f4195092be2f2
Yusif Aliyev <114253515+YusifAliyevPro@users.noreply.github.com>
2026-02-19 23:41:45 +0400

    fix: import React in layout and ensure visionTool is correctly imported in sanity config

--- src/app/studio/layout.tsx
+++ src/app/studio/layout.tsx
@@ -1,7 +1,10 @@
 import { ReactNode, Suspense } from "react";
+import React from "react";
 import "@/lib/env";
 import { inter, poppins } from "@/lib/fonts";
 import "../globals.css";
+
+globalThis.React = React;
 
 export default function RootLayout({ children }: { children: Readonly<ReactNode> }) {
   return (
