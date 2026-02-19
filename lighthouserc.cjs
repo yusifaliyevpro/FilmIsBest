d25b16765765b51b39842fb7b8f636f81b1ee729
Yusif Aliyev <114253515+YusifAliyevPro@users.noreply.github.com>
2026-02-19 23:56:01 +0400

    fix: add LHCI_GITHUB_APP_TOKEN to CI workflow and update lighthouserc.cjs configuration

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,5 +1,10 @@
 module.exports = {
   ci: {
+    collect: {
+      url: ["http://localhost:3000"],
+      startServerCommand: "npm run start",
+      startServerReadyPattern: "Ready in",
+    },
     upload: {
       target: "temporary-public-storage",
     },
