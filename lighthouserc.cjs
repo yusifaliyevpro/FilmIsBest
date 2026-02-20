968ce2400ca489622be134c3730268cb8486309c
Yusif Aliyev <114253515+YusifAliyevPro@users.noreply.github.com>
2026-02-20 23:13:40 +0400

    fix: remove numberOfRuns configuration from Lighthouse CI settings

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -4,7 +4,6 @@ module.exports = {
       url: ["http://localhost:3000/en", "http://localhost:3000/en/movies", "http://localhost:3000/en/about"],
       startServerCommand: "pnpm start",
       startServerReadyPattern: "Ready in",
-      numberOfRuns: 1,
     },
     upload: {
       target: "temporary-public-storage",
