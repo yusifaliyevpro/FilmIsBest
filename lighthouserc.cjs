3452eec0e113609faa2da5467b2e42b15c5b212d
Yusif Aliyev <114253515+YusifAliyevPro@users.noreply.github.com>
2026-02-20 15:01:04 +0400

    fix: update URLs in lighthouserc.cjs and refactor imports in FormSubmit component

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,7 +1,12 @@
 module.exports = {
   ci: {
     collect: {
-      url: ["http://localhost:3000"],
+      url: [
+        "http://localhost:3000/en",
+        "http://localhost:3000/en/movies",
+        "http://localhost:3000/en/about",
+        "http://localhost:3000/en/movies/first-signal",
+      ],
       startServerCommand: "npm run start",
       startServerReadyPattern: "Ready in",
     },
